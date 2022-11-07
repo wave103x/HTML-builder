47bbc7a76c984b8f7966e7459d281422f7ae04b7
Mikhail Sprygin <wave103x@gmail.com>
2022-11-07 17:34:19 +0300

    fix: 04 remove redundant files via copying

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,7 @@ const fs = require('fs/promises');
 
 async function copyDir() {
   try {
+    await fs.rm(path.join(__dirname, 'files-copy'), {recursive: true, force: true});
     fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
     const files = await fs.readdir(path.join(__dirname, 'files'));
     for (const file of files) {
